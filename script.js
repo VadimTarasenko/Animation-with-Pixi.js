9dea429b0481b032b5a5988462ecc0d8d04e2854
Vadim Tarasenko <vadym.tarasenko99@gmail.com>
2019-10-21 22:10:01 +0300

    update

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 const app = new PIXI.Application({
-  height: window.innerHeight,
-  width: window.innerWidth
+  height: window.innerHeight,   // висота канваса
+  width: window.innerWidth      // ширина канваса
 });
-const loader = new PIXI.Loader();
-const container = new PIXI.Container();
+const loader = new PIXI.Loader(); // об'єкт для загрузки і збереження спрайтів (картинок)
+const container = new PIXI.Container(); // сукупність об'єктів (фоток, фільтрів ...)
 
-let posX, displacementSprite, displacementFilter, bg, vx;
+let posX, displacementSprite, displacementFilter, vx;
 
-document.body.appendChild(app.view);
+document.body.appendChild(app.view); // добавляємо до body - canvas
 
-app.stage.interactive = true;
+app.stage.interactive = true; // дозволити події на канвасі ("mousemove" на 38 строчці)
 app.stage.addChild(container);
 
 
-loader.add("img/gradient4.png").add("img/bg.jpg").load(setup);
+loader.add("img/gradient4.png").add("img/gradient_large.png").add("img/bg.jpg").load(setup); // загружаємо картинки. Після загрузки - запускаємо ф-цію "setup"
 
 function setup() {
-  posX = app.renderer.width / 2;
-  displacementSprite = new PIXI.Sprite(loader.resources["img/gradient4.png"].texture);
-  displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+  posX = app.renderer.width / 2; // центр канваса
+
+  displacementSprite = new PIXI.Sprite(loader.resources["img/gradient_large.png"].texture); // спрайт, на основі якого буде створений фільтер
+  displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite); // 
   displacementSprite.anchor.set(0.5);
   displacementSprite.x = app.renderer.width / 2;
   displacementSprite.y = app.renderer.height / 2;
+
   vx = displacementSprite.x;
   app.stage.addChild(displacementSprite);
-  container.filters = [displacementFilter];
+  container.filters = [displacementFilter, blurFilter];
   displacementFilter.scale.x = 0;
   displacementFilter.scale.y = 0;
-  bg = new PIXI.Sprite(loader.resources["img/bg.jpg"].texture);
+
+  const bg = new PIXI.Sprite(loader.resources["img/bg.jpg"].texture);
   bg.width = app.renderer.width;
   bg.height = app.renderer.height;
   container.addChild(bg);
+
   app.stage.on("mousemove", onPointerMove).on("touchmove", onPointerMove);
+  
   loop();
 }
 
